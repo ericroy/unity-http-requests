a3ab18ba48cf27d89050755d4b3d38685509fcc4
Eric Roy <ericsroy@gmail.com>
2021-01-04 22:23:05 -0800

    wip

--- objc~/src/ResultStorage.h
+++ objc~/src/ResultStorage.h
@@ -1,11 +1,12 @@
 #import <Foundation/Foundation.h>
 #include "UnityHttpRequests.h"
+#include "Result.h"
 
 @interface ResultStorage : NSObject {
     NSMutableArray *headers; // HeaderStorage[]
     UHR_Header *headerRefs; // UHR_Header[]
     NSData *body;
 }
-- (id)init;
+- (id)initWithResult:(Result *)result;
 - (void)dealloc;
 @end
