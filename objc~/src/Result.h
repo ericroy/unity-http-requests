35d4f59a099ce6a62d080a7abbdb84586c0f64ee
Eric Roy <ericsroy@gmail.com>
2021-01-04 22:21:09 -0800

    wip

--- objc~/src/Result.h
+++ objc~/src/Result.h
@@ -7,6 +7,6 @@
     NSHTTPURLResponse *response;
     NSError *error;
 }
-- (id)init;
+- (id)initWithRid:(NSInteger)aRid response:(NSHTTPURLResponse *)aResponse data:(NSData *)aData error:(NSError *)aError;
 - (void)dealloc;
 @end
