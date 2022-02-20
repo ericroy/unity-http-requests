09d965f987263bb32bffaa6abe5eb2c50d22dcda
Eric Roy <ericsroy@gmail.com>
2022-02-19 16:03:40 -0800

    wip

--- uhr/objc/src/Logging.m
+++ uhr/objc/src/Logging.m
@@ -10,26 +10,26 @@ LogSink* gLogSink = [[LogSink alloc] init];
 - (id)init {
     self = [super init];
     if (self) {
-        self._callback = nil;
-        self._userData = nil;
+        self->_callback = nil;
+        self->_userData = nil;
     }
     return self;
 }
 
 - (void)set:(UHR_LoggingCallback)callback userData:(void *)userData {
     @synchronized(self) {
-        self._callback = callback;
-        self._userData = userData;
+        self->_callback = callback;
+        self->_userData = userData;
     }
 }
 
 - (void)log:(NSString*)str {
     @synchronized(self) {
-        if (self._callback != nil) {
+        if (self->_callback != nil) {
             UHR_StringRef ref;
             ref->characters = (const uint16_t*)CFStringGetCharactersPtr((__bridge CFStringRef)str);
             ref->length = (uint32_t)str.length;
-            self._callback(ref, self._userData);
+            self->_callback(ref, self._userData);
         }
     }
 }
